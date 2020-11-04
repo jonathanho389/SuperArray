cd9542cffa6ecb0e665d34fd70c6d3427f89ee37
Jonathan Ho <hojon389@gmail.com>
2020-11-04 09:53:32 -0500

    resize works and modified add

--- SuperArray.java
+++ SuperArray.java
@@ -1,33 +1,32 @@
 public class SuperArray{
   private String[] data;
   private int size;
+  private int capacity;
 
   private void resize(){
     String[] newArr = new String[data.length + 1];
     for(int i = 0;i < size;i++){
       newArr[i] = data[i];
     }
+    data = newArr;
+    capacity++;
   }
   public SuperArray(){
     data = new String[10];
     size = 0;
+    capacity = 10;
   }
   public int size(){
     return size;
   }
 
   public boolean add(String element){
-    if(size >= 10){
+    if(size >= capacity){
       resize();
+    }
       data[size] = element;
       size++;
       return true;
-    }
-    else{
-      data[size] = element;
-      size++;
-      return true;
-    }
   }
   public String get(int index){
     return data[index];
